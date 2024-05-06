6cee5560606d32ce2010894d873e0ac41e8852e8
kotnarkot <kotnarkot.aregeon@mail.ru>
2024-05-07 02:19:39 +1000

    Что-то такое

---
+++ bototttotototo/Game/Dungeon/Monster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bototttotototo.Game.Dungeon
+{
+    public class Monster
+    {
+        public EventHandler<Monster> OnDied;
+        public EventHandler<Monster> OnAction;
+        public EnemyStats Stats { get; private set; }
+        public Monster(EnemyStats stats)
+        {
+            Stats = stats;
+        }
+
+        public void TakeDamage()
+        {
+
+        }
+        public void Act()
+        {
+            OnAction.Invoke(this, this);
+        }
+
+        public void Die()
+        {
+            OnDied.Invoke(this, this);
+        }
+    }
+}
--- bototttotototo/Game/Dungeon/Room.cs
+++ bototttotototo/Game/Dungeon/Room.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bototttotototo.Game
+namespace bototttotototo.Game.Dungeon
 {
-    public class Formuls
+    public class Room
     {
-        
+
     }
 }
